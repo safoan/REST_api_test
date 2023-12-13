f495e083c3f93d010bf4798209978dc58a116a78
safoan bazine <safibazine@gmail.com>
2023-12-13 20:49:16 +0000

    added the rest api files

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 
+import router from './router';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -19,5 +21,13 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-    console.log('Server running on http://localhost:8080/');
-  });
+  console.log('Server running on http://localhost:8080/');
+});
+
+const MONGO_URL = ''; // DB URI
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
